5eb7636e18a5bf450908249d447419b470f54d26
Panayioplay <panayiotis1rizos@gmail.com>
2025-06-04 18:21:13 +0200

    Update idk.py

--- idk.py
+++ idk.py
@@ -1,1 +1,10 @@
-print("hello")
+def nuke(n):
+    a = []
+    for i in range(10):
+        if n > 1:
+            a.append(nuke(n-1))
+        else:
+            a.append(n)
+    return a
+    
+print(nuke(7))
